3e9a3421affeb2bd1f053a594d37385725e98e21
Ifunanya <ifyokechukwu@yahoo.com>
2024-07-22 11:06:27 -0500

    corrected source path

--- src/main.tf
+++ src/main.tf
@@ -1,10 +1,10 @@
 module "ec2" {
-    source = ../a-module/ec2
+    source = "../a-module/ec2"
     ami = "ami-0ff591da048329e00"
     instance_type = "t2.micro"
 }
 
 module "s3" {
-    source  = ../a-module/s3
+    source  = "../a-module/s3"
     bucket = "fantasticbucket-cicd24"
 }
